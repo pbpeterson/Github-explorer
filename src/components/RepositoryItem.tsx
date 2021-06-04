1231c3f1b8e80c6f4581f805cb0973ec2a0270c9
Peterson Pereira <petersonbozza7@gmail.com>
2021-06-04 16:23:04 -0300

    adicionado primeiro projeto Ignite ReactJS

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,21 @@
+interface RepositoryItemProps{
+  repository: {
+    name: string;
+    description: string;
+    html_url: string
+  }
+}
+
+
+export const RepositoryItem = ( props : RepositoryItemProps) => {
+  return (
+    <li>
+      <strong>{props.repository.name}</strong>
+      <p>{props.repository.description}</p>
+
+      <a href={props.repository.html_url} target="_blank">
+        Acessar Repositório
+      </a>
+    </li>
+  );
+};
